29c4da8c318e012abae21941700b2b24f85676de
unknown <yuxi@YUXMED0122L.internal.apexcorp.local>
2018-02-07 16:36:55 -0500

    working on day2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,20 @@ export class AppComponent {
   count = 0;
   imageSource = './../favicon.ico';
   status = false;
-  changeStatus(){
-    this.status = !this.status;
+  imageUrl;
+  imagePath = "https://static.pexels.com/photos/34950/pexels-photo.jpg";
+  baseUrl: 'https://api.adorable.io/avatars/500/';
+  urlSuffix: '@adorable.io.png';
+  
+  generateRandomFace(num) {
+    this.imageUrl = `${this.baseUrl}${num}${this.urlSuffix}`;
   }
+  constructor(){
+    setInterval(function(){
+      let num = Math.random();
+      let o = this.generateRandomFace(num);
+    }, 1000);
+  }
+
+ 
 }
