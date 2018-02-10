176566b534b8b36a36d992dd4538265996b8da5e
unknown <yuxi@YUXMED0122L.internal.apexcorp.local>
2018-02-10 09:46:19 -0500

    Finished day 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,24 +9,21 @@ export class AppComponent {
   title = 'My application';
   author = 'StevenSanz';
   count = 0;
-  imageSource = './../favicon.ico';
-  status = false;
-  imageUrl;
-  imagePath = "https://static.pexels.com/photos/34950/pexels-photo.jpg";
-  baseUrl: 'https://api.adorable.io/avatars/500/';
-  urlSuffix: '@adorable.io.png';
+  imageSource = './favicon.ico';
+  enabled = false;
+  @Input() imageUrl = "https://api.adorable.io/avatars/500/300@adorable.io.png";
+  baseUrl= 'https://api.adorable.io/avatars/500/';
+  urlSuffix= '@adorable.io.png';
   num;
   constructor(){
     setInterval( () =>{
-      this.num = Math.floor(Math.random() + 1);
+      this.num = Math.floor((Math.random() * 10) + 1);;
       this.generateRandomFace(this.num);
     }, 1000);
   }
 
   generateRandomFace(num) {
-    console.log('jjj', num);
-    
-    this.imageUrl = `${this.baseUrl}${num}${this.urlSuffix}`;
+    this.imageUrl = `${this.baseUrl}${this.num}${this.urlSuffix}`;
   }
  
 }
